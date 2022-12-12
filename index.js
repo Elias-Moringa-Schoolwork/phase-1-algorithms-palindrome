7ab332556b42d05f8ca164cba260038c06963b02
eliasndegwa <eliasndegwamwai@gmail.com>
2022-12-12 15:41:01 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const reverse=word.split('').reverse().join('');
+  if(word===reverse){
+    return true
+  }else{
+    return false
+  }
 }
-
+isPalindrome('')
 /* 
   Add your pseudocode here
+  create isPalindrome(word) function
+  const reverse=word.split('').reverse().join('');
+  if word===reverse eturn true, else return false
 */
 
 /*
   Add written explanation of your solution here
+  -initialize a function isPalindrome with a string argument called word
+  -initialize a value reverse
+  -create an array of individual values in the word array using split() function
+  -reverse the order of the values with reverse() function
+  -join the values back together using join() function
+  -check if word is a palindrome using ifelse statement
 */
 
 // You can run `node index.js` to view these console logs
